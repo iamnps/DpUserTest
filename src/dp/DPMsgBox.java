98a99019ff5c9528d7f8f6b03af9aa535c63fd6a
iamnps <niepeishen@sina.com>
2016-05-13 10:22:13 +0800

    test

--- src/dp/DPMsgBox.java
+++ src/dp/DPMsgBox.java
@@ -4,7 +4,7 @@ import javax.swing.JFrame;
 public class DPMsgBox extends JFrame {
 	
 	/**
-	 * 这个类只是提示而且，你可以不这么些，也可以扩展，甚至加入语音提示都行
+	 * 这个类只是提示而且，你可以扩展，甚至加入语音提示都行
 	 */
 	private static final long serialVersionUID = 1L;
 
